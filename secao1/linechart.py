80077d31a9098f08b5d77aeae79ad000cead16f6
ftrajano <ftrajano@gmail.com>
2022-01-04 16:04:26 -0300

    update linechart.py linechart.png

--- secao1/linechart.py
+++ secao1/linechart.py
@@ -11,7 +11,9 @@ trace0 = go.Scatter(x=x_values, y=y_values+5,
 
 trace1 = go.Scatter(x=x_values, y=y_values, mode='lines', name='mylines')
 
-data = [trace0, trace1]
+trace2 = go.Scatter(x=x_values, y=y_values-5, mode='lines+markers', name='my favorite')
+
+data = [trace0, trace1, trace2]
 
 layout = go.Layout(title='Line Charts')
 
